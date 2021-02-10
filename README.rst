3a883079728c4eb61267643c24b07441832f197e
lagravasandovald <daniel.lagravasandoval@niwa.co.nz>
2021-02-11 11:05:08 +1300

    Fixed some issues to be able to install as package

--- README.rst
+++ README.rst
@@ -1,11 +1,11 @@
-NetCDF extraction tool
+NetCDF extraction util
 ----------------------
 
 This is a simple script to extract time-series from a NetCDF file to a Comma Separated Value (CSV) file.
 
 To use:
 
-    netcdf_extraction_code <input_netcdf_file> <selected_reach>
+    netcdf_extraction_util.py <input_netcdf_file> <selected_reach>
 
 This will produce a <selected_reach>.csv file with columns date and value for the selected reach. If the
 reach does not exist on <input_netcdf_file>, there will be an error.
--- setup.py
+++ setup.py
@@ -1,19 +1,24 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 def readme():
     with open('README.rst') as f:
         return f.read()
 
 
-setup(name="netcdf_extraction", version="0.1", 
+setup(name="netcdf_extraction_util", version="0.1", 
       description="Script to extract time-series from Netcdf",
       long_description=readme(),
       author="Daniel Lagrava",
       author_email="daniel.lagravasandoval@niwa.co.nz",
       license="GNU",
-      scripts=['bin/extract_time_series.py'],
+      packages=["netcdf_extraction_util"],
+      entry_points={
+        'console_scripts': [
+            'netcdf_extraction_util=netcdf_extraction_util.command_line:main'
+        ],
+      },
       install_requires=['numpy','netCDF4','xarray'],
-      include_pachage_data=True,
+      include_package_data=True,
       zip_safe=False
 )
 
